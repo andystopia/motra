9bf79d6b5d7467606442546c9e8f2e6775b3cd10
Quan Phan <hoangquanphan2906@gmail.com>
2022-02-01 12:00:01 -0500

    Rewrite trajectory visualization with matplotlib

--- motra/__init__.py
+++ motra/__init__.py
@@ -1,4 +1,4 @@
-from .trajectory import arena_trajectory
+from .trajectory import arena_trajectory, heatmap
 from .arena import arena
 from .parse import parse
 from .stats import stats, visualize_stats
